2a440a31153a37586a366239f798304ed156695a
prasim <prasim.jain@sap.com>
2018-01-10 20:49:17 +0530

    Removing unused code

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,28 +3,16 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
-import { HeroesComponent } from './heroes/heroes.component';
-import { HeroDetailComponent } from './hero-detail/hero-detail.component';
-import { HeroService } from './hero.service';
-import { MessageService } from './message.service';
-import { MessagesComponent } from './messages/messages.component';
-import { AppRoutingModule } from './/app-routing.module';
-import { DashboardComponent } from './dashboard/dashboard.component';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    HeroesComponent,
-    HeroDetailComponent,
-    MessagesComponent,
-    DashboardComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
-    FormsModule,
-    AppRoutingModule
+    FormsModule
   ],
-  providers: [HeroService, MessageService],
+  providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
